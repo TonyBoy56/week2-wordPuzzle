4060122d31388566706ab59638126b4acd3d3f3e
josh <jhell85@gmail.com>
2020-03-31 15:59:51 -0700

    added a different type of branching to get prime numbers

--- js/scripts.js
+++ js/scripts.js
@@ -2,23 +2,33 @@ $(document).ready(function() {
   //on click function
   $("form").submit(function(event) {
     event.preventDefault();
+    console.log("form submited");
+
     const number = $("#input1").val();
     let numbersArray = [];
     let primeArray = [];
     let counter = 0;
     let currentNumber;
-    while (counter < number) {
-      counter++;
-      numbersArray.push(counter);
-		}
-		for (let i = 0; i < numbersArray.length; i++) {
-      if (numbersArray[i] < 2) {
-        primeArray.push(numbersArray[i]);
-        numbersArray.splice(i,1)
-      } else 
-      currentNumber = numbersArray[i]
-      return
+    if (number === 1) {
+      numbersArray.push(1);
+    } else if (number === "2") {
+      numbersArray.push(1, 2);
+    } else {
+      while (counter < number) {
+        counter++;
+        numbersArray.push(counter);
+      }
+      primeArray.push(numbersArray.shift())
+      counter = 2;
+      // while (counter < 11) {
+      //   for (let i = 1; i < numbersArray.length; i++) {
+      //     const element = array[i];
+          
+      //   }
+      // }
+      console.log(
+        `numbersArray = ${numbersArray} primeArray= ${primeArray} currentNumber ${currentNumber}`
+      );
     }
-    console.log(`numbersArray = ${numbersArray} primeArray= ${primeArray}`);	
-		});
   });
+});
