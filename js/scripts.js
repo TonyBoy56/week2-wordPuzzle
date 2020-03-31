1e447282d09ba5ca1e42ad36ffa4e02a9ed71ff3
josh <jhell85@gmail.com>
2020-03-31 14:28:38 -0700

    added function for Prime sifting exercise

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,22 @@
 $(document).ready(function() {
-
-	//on click function
-	$('form').submit(function(event) {
-	// 	event.preventDefault();
-	// 	let stringInput = $("#input1").val().split("");
-  //   let puzzelArray = stringInput.map(function(letter) {
-  //     if (letter === "a" || letter === "A" || letter === "e" || letter === "E" || letter === "i" || letter === "I" || letter === "o" || letter === "O" || letter === "u" || letter === "U"){
-	// 			return "-";
-  //     }
-  //     return letter
-  //   })
-	// 	console.log(puzzelArray);
-    
-
-	// });
+  //on click function
+  $("form").submit(function(event) {
+    event.preventDefault();
+    // let stringInput = $("#input1").val().split("");
+    // let puzzelArray = stringInput.map(function(letter) {
+    //   if (letter === "a" || letter === "A" || letter === "e" || letter === "E" || letter === "i" || letter === "I" || letter === "o" || letter === "O" || letter === "u" || letter === "U"){
+    // 		return "-";
+    //   }
+    //   return letter
+    // })
+    // console.log(puzzelArray);
+    const number = $("#input1").val();
+    const numbersArray = [];
+    let counter = 0;
+    while (counter < number) {
+      counter++;
+      numbersArray.push(counter);
+    }
+    console.log(numbersArray);
+  });
 });
